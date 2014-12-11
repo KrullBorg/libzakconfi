d29278f52353c8d76649475e77909a64d1ce7a3f
Andrea Zagli <azagli@libero.it>
2014-12-11 17:59:59 +0100

    Started implementation of plugins.

---
+++ src/confipluggable.c
@@ -0,0 +1,101 @@
+/*
+ * confipluggable.c
+ * This file is part of libconfi
+ *
+ * Copyright (C) 2014 Andrea Zagli <azagli@libero.it>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Library General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "confipluggable.h"
+
+/**
+ * SECTION:confipluggable
+ * @short_description: Interface for pluggable plugins.
+ * @see_also: #PeasExtensionSet
+ *
+ **/
+
+G_DEFINE_INTERFACE(ConfiPluggable, confi_pluggable, G_TYPE_OBJECT)
+
+void
+confi_pluggable_default_init (ConfiPluggableInterface *iface)
+{
+	static gboolean initialized = FALSE;
+
+	if (!initialized)
+		{
+			/**
+			* ConfiPluggable:name:
+			*
+			*/
+			g_object_interface_install_property (iface,
+			                                     g_param_spec_string ("name",
+			                                                          "Configuraton Name",
+			                                                          "The configuration name",
+			                                                          "",
+			                                                          G_PARAM_READWRITE | G_PARAM_CONSTRUCT));
+
+			/**
+			* ConfiPluggable:description:
+			*
+			*/
+			g_object_interface_install_property (iface,
+			                                     g_param_spec_string ("description",
+			                                                          "Configuraton Description",
+			                                                          "The configuration description",
+			                                                          "",
+			                                                          G_PARAM_READWRITE | G_PARAM_CONSTRUCT));
+
+			/**
+			* ConfiPluggable:root:
+			*
+			*/
+			g_object_interface_install_property (iface,
+			                                     g_param_spec_string ("root",
+			                                                          "Configuraton Root",
+			                                                          "The configuration root",
+			                                                          "/",
+			                                                          G_PARAM_READWRITE | G_PARAM_CONSTRUCT));
+
+			initialized = TRUE;
+		}
+}
+
+/**
+ * confi_pluggable_activate:
+ * @pluggable: A #ConfiPluggable.
+ * @cnc_string: The connection string.
+ *
+ * Initialize the backend.
+ *
+ * Returns: #TRUE if success.
+ */
+gboolean
+confi_pluggable_initialize (ConfiPluggable *pluggable, const gchar *cnc_string)
+{
+  ConfiPluggableInterface *iface;
+
+  g_return_if_fail (CONFI_IS_PLUGGABLE (pluggable));
+
+  iface = CONFI_PLUGGABLE_GET_IFACE (pluggable);
+  g_return_if_fail (iface->initialize != NULL);
+
+  return iface->initialize (pluggable, cnc_string);
+}
---
+++ src/confipluggable.h
@@ -0,0 +1,69 @@
+/*
+ * confipluggable.h
+ * This file is part of libconfi
+ *
+ * Copyright (C) 2014 - Andrea Zagli <azagli@libero.it>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Library General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef __CONFI_PLUGGABLE_H__
+#define __CONFI_PLUGGABLE_H__
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+/*
+ * Type checking and casting macros
+ */
+#define CONFI_TYPE_PLUGGABLE             (confi_pluggable_get_type ())
+#define CONFI_PLUGGABLE(obj)             (G_TYPE_CHECK_INSTANCE_CAST ((obj), CONFI_TYPE_PLUGGABLE, ConfiPluggable))
+#define CONFI_PLUGGABLE_IFACE(obj)       (G_TYPE_CHECK_CLASS_CAST ((obj), CONFI_TYPE_PLUGGABLE, ConfiPluggableInterface))
+#define CONFI_IS_PLUGGABLE(obj)          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), CONFI_TYPE_PLUGGABLE))
+#define CONFI_PLUGGABLE_GET_IFACE(obj)   (G_TYPE_INSTANCE_GET_INTERFACE ((obj), CONFI_TYPE_PLUGGABLE, ConfiPluggableInterface))
+
+/**
+ * ConfiPluggable:
+ *
+ * Interface for pluggable plugins.
+ */
+typedef struct _ConfiPluggable           ConfiPluggable; /* dummy typedef */
+typedef struct _ConfiPluggableInterface  ConfiPluggableInterface;
+
+/**
+ * ConfiPluggableInterface:
+ * @g_iface: The parent interface.
+ * @initialize: Construct the plugin.
+ *
+ * Provides an interface for pluggable plugins.
+ */
+struct _ConfiPluggableInterface {
+	GTypeInterface g_iface;
+
+	/* Virtual public methods */
+	gboolean (*initialize) (ConfiPluggable *pluggable, const gchar *cnc_string);
+};
+
+/*
+ * Public methods
+ */
+GType confi_pluggable_get_type (void) G_GNUC_CONST;
+
+gboolean confi_pluggable_initialize (ConfiPluggable *pluggable, const gchar *cnc_string);
+
+G_END_DECLS
+
+#endif /* __CONFI_PLUGGABLE_H__ */
